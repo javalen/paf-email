2bd6a3a45ea6ec34e32f3e6dc72e4e800050d1e0
javalen <lenflour@gmail.com>
2024-06-16 13:57:14 -0700

    added verify

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const PocketBase = require("pocketbase/cjs");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-
+const pb = new PocketBase("https://predictiveaf-dev.fly.dev/");
 // Configure the email transporter
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
@@ -19,19 +20,21 @@ const transporter = nodemailer.createTransport({
 });
 
 // Email template
-const emailTemplate = (name, message) => `
-    <h1>Hello, ${name}</h1>
-    <p>${message}</p>
+const emailTemplate = (name, verificationLink) => `
+<h1>Hello, ${name}</h1>
+<p>Please click the link below to verify your email address:</p>
+<a href="${verificationLink}" style="display: inline-block; padding: 10px 20px; font-size: 16px; color: #fff; background-color: #007bff; text-decoration: none; border-radius: 5px;">Verify Email</a>
 `;
 
 app.post("/send-email", (req, res) => {
   const { to, subject, name, message } = req.body;
   console.log(to, subject, name, message);
+  const verificationLink = `https://paf-email.onrender.com/verify-email?token=${to}`;
   const mailOptions = {
     from: "support@predictiveaf.com",
     to: to,
     subject: subject,
-    html: emailTemplate(name, message),
+    html: emailTemplate(name, verificationLink),
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
@@ -41,6 +44,53 @@ app.post("/send-email", (req, res) => {
     res.status(200).send("Email sent: " + info.response);
   });
 });
+
+app.post("/verify-email", async function (req, res) {
+  const { token } = req.query;
+  console.log("Token" + token);
+  await verifyUser(token);
+
+  res.status(200).send("User Verified: ");
+});
+
+const verifyClient = async (id) => {
+  try {
+    console.log("Updating client");
+    const client = await pb
+      .collection("client")
+      .getFirstListItem(`manager="${id}"`);
+    console.log("Got the client", client.name);
+    const record = await pb
+      .collection("client")
+      .update(client.id, { verified: true });
+    console.log("Client verified:", record.verified);
+    return record;
+  } catch (error) {
+    console.log(`Error verifying Client ${error}`);
+  }
+};
+
+const verifyUser = async (email) => {
+  try {
+    const user = await pb
+      .collection("users")
+      .getFirstListItem(`email="${email}"`);
+
+    console.log("User", user.name);
+    const personel = await pb
+      .collection("personel")
+      .getFirstListItem(`user="${user.id}"`);
+    console.log("Personel", personel.full_name);
+    const record = await pb
+      .collection("personel")
+      .update(personel.id, { verified: true });
+    console.log("Personel verified", record);
+    const done = await verifyClient(user.id);
+    return done;
+  } catch (error) {
+    console.log(`Error verifying User ${error}`);
+  }
+};
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
